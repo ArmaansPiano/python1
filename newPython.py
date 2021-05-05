b17bce67076689cef6e34991dabc357720eb5193
ArmaansPiano <71108324+ArmaansPiano@users.noreply.github.com>
2021-05-04 21:17:32 -0500

    Add files via upload

---
+++ newPython.py
@@ -0,0 +1,7 @@
+pocketMoney = int(input("Enter the pocket money you earn."))
+if(pocketMoney > 500):
+    print("Congrats!")
+elif(pocketMoney > 100):
+    print("Good job!")
+else: 
+    print("Keep working!")
